69f632482439e2e8902f8a6c16dc01cb986db293
superpuper32 <pochitalin_m@mail.ru>
2023-12-23 00:00:56 +0300

    Readme link added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
 import './globals.css'
-import { Providers } from './providers';
+import { Providers } from './providers'
 
 import Nav from './components/nav'
 
